93635e0337c2814367489565b3c47bc34501689a
Nguyen Minh Trung <nguyenminhtrung18072004@gmail.com>
2025-11-19 18:55:29 +0700

    permantly command handler event

--- Services/GradingSystem.Services.Submission.Api/Services/ExamCreatedHandler.cs
+++ Services/GradingSystem.Services.Submission.Api/Services/ExamCreatedHandler.cs
@@ -13,19 +13,19 @@ public class ExamCreatedHandler
 
     public async Task Handle(ExamCreated message)
     {
-        bool exists = await _db.Exams.AnyAsync(x => x.Id == message.Id);
-        if (exists)
-            return;
+        //bool exists = await _db.Exams.AnyAsync(x => x.Id == message.Id);
+        //if (exists)
+        //    return;
 
-        var Exam = new Exam
-        {
-            Id = message.Id,
-            Titile = message.Title,
-            SemesterId = message.SemesterId,
-            DueDate = message.DueDate
-        };
+        //var Exam = new Exam
+        //{
+        //    Id = message.Id,
+        //    Titile = message.Title,
+        //    SemesterId = message.SemesterId,
+        //    DueDate = message.DueDate
+        //};
 
-        _db.Exams.Add(Exam);
-        await _db.SaveChangesAsync();
+        //_db.Exams.Add(Exam);
+        //await _db.SaveChangesAsync();
     }
 }
