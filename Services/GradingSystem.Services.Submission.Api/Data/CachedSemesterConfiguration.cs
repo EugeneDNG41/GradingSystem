9c0d0a52ef04ed1acc47ffd4a1ff035303423fb9
Ngoc Han <phngochan1301@gmail.com>
2025-11-19 19:12:56 +0700

    update cached semester config

---
+++ Services/GradingSystem.Services.Submission.Api/Data/CachedSemesterConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GradingSystem.Services.Submissions.Api.Data
+{
+    public class CachedSemesterConfiguration : IEntityTypeConfiguration<CachedSemester>
+    {
+        public void Configure(EntityTypeBuilder<CachedSemester> builder)
+        {
+            builder.ToTable("CachedSemesters");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+
+}
